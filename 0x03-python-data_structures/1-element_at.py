c8ed2ded4663ac12f7c2341969cefe4c8214fa50
joshua-983 <officialgh983@gmail.com>
2023-09-10 11:16:33 -0700

    element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,7 @@
 # 1-element_at.py
 
 def element_at(my_list, idx):
+    """retieve an element from a list"""
     if idx < 0 or idx > (len(my_list) - 1):'''collecting data from a list'''
         return None
     return (my_list[idx])
