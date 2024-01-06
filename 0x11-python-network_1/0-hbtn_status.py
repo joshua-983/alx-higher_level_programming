d3f5af5f0b0b64b1592d51dca5756e1f8c098a80
joshua-983 <officialgh983@gmail.com>
2024-01-06 12:48:00 -0800

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""0x11. Python - Network #1, task 0. What's my status? #0
+"""Fetches the URL: https://intranet.hbtn.io/status
 """
 
+from urllib.request import Request, urlopen
+
+
 if __name__ == "__main__":
-    # Importing the request module from urllib library
-    from urllib import request
+    req = Request('https://intranet.hbtn.io/status')
 
-    # Opening a connection to 'https://intranet.hbtn.io/status'
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        # Reading the response data
-        html = response.read()
+    with urlopen(req) as res:
+        content = res.read()
+        utf8_content = content.decode('utf-8')
 
-    # Printing information about the response
-    print('Body response:')
-    print('\t- type: {}'.format(type(html)))  # Printing type of data
-    print('\t- content: {}'.format(html))  # Printing content of response
-    print('\t- utf8 content: {}'.format(html.decode('utf-8')))  # Decoding and printing content in UTF-8
-    # The charset can be obtained using response.headers.get_content_charset()
-
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(content)))
+        print('\t- content: {_content}'.format(_content=content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
